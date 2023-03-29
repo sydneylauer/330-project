87f25d3f4b1fd68c3434f14e92eb0263624f2276
sydney-lauer <Sydney.L@bubblez.life>
2023-03-29 15:55:21 -0400

    Initial commit

---
+++ project.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+
+demographics_df = pd.read_csv("acs_demographics.csv")
+income_df = pd.read_csv("acs_income.csv")
+work_df = pd.read_csv("acs_work.csv")
+calls911_df = pd.read_csv("Detroit-911_Calls_for_Service.csv")
+fires_df = pd.read_csv("Detroit-Fire_Incidents.csv")
+
